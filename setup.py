a2fed0be3d61b26576cc2690ba3ce3c92624ff7c
Thomas Klijnsma <klijnsma@fnal.gov>
2019-10-11 07:40:53 -0500

    updated version no

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='lpcdbeclient',
-    version='0.5',
+    version='0.6',
     license='BSD 3-Clause License',
     description='Client for inferencing the LPC DBE',
     url='https://github.com/tklijnsma/lpcdbeclient.git',
-    download_url='https://github.com/tklijnsma/lpcdbeclient/archive/v0_5.tar.gz',
+    download_url='https://github.com/tklijnsma/lpcdbeclient/archive/v0_6.tar.gz',
     author='Thomas Klijnsma',
     author_email='tklijnsm@gmail.com',
     packages=['lpcdbeclient'],
